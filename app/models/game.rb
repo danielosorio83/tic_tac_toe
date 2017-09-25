6254b97bf8d59fc403bba09e45f367209690d528
Daniel Osorio <danielosorio83@gmail.com>
2017-09-25 07:44:02 -0500

    initialize game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,34 @@
 class Game
-  attr_accessor :size
+  attr_accessor :size, :table_size, :player, :table, :movements
 
-  def initialize(size = nil)
-    self.size = size
+  NEUTRO    = '-'
+  PLAYER_1  = 'X'
+  PLAYER_2  = 'O'
+  PLAYERS   = [PLAYER_1, PLAYER_2]
+
+  def initialize(initial_size = 3)
+    self.size = initial_size
+    self.table_size = size * size
+    self.player = PLAYERS.sample
+    self.table = Array.new(table_size, NEUTRO)
+    self.movements = []
+    move(initial_position) if player == PLAYER_2
+  end
+
+  def move(position)
+    self.table[position] = player
+    self.player = other_player
+    self.movements << position
+    self
+  end
+
+  private
+
+  def initial_position
+    size == 3 ? 4 : 0
+  end
+
+  def other_player
+    player == PLAYER_1 ? PLAYER_2 : PLAYER_1
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,14 +3,78 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   # Accessors
   it { is_expected.to respond_to(:size) }
+  it { is_expected.to respond_to(:table_size) }
+  it { is_expected.to respond_to(:player) }
+  it { is_expected.to respond_to(:table) }
+  it { is_expected.to respond_to(:movements) }
 
   let(:size) { 3 }
   let(:game) { Game.new(size) }
 
   describe 'Public Instance Methods' do
-    describe '#initialize(size = 3)' do
+    describe '#initialize(initial_size = 3)' do
       it 'setup a size' do
         expect(game.size).to eq(size)
+      end
+
+      it 'setup a new table size' do
+        expect(game.table_size).to eq(size * size)
+      end
+
+      it 'setup a new table' do
+        allow(Game::PLAYERS).to receive(:sample).and_return(Game::PLAYER_1)
+        expect(game.table).to eq(Array.new(game.table_size, Game::NEUTRO))
+      end
+
+      it 'setup a player' do
+        expect(game.player).to be_in(Game::PLAYERS)
+      end
+
+      it 'setup a movements' do
+        allow(Game::PLAYERS).to receive(:sample).and_return(Game::PLAYER_1)
+        expect(game.movements).to eq([])
+      end
+
+      context 'when `player` is `PLAYER_2`' do
+        it 'call `move`' do
+          allow(Game::PLAYERS).to receive(:sample).and_return(Game::PLAYER_2)
+          expect_any_instance_of(Game).to receive(:move)
+          game
+        end
+      end
+    end
+  end
+
+  describe 'Private Instance Methods' do
+    describe '#initial_position' do
+      context 'when `size` is 3' do
+        it 'returns 4' do
+          expect(game.send(:initial_position)).to eq(4)
+        end
+      end
+
+      context 'when `size` is 4' do
+        let(:size) { 4 }
+
+        it 'returns 0' do
+          expect(game.send(:initial_position)).to eq(0)
+        end
+      end
+    end
+
+    describe '#other_player' do
+      context 'when `player` is `PLAYER_1`' do
+        it 'returns `PLAYER_2`' do
+          game.player = Game::PLAYER_1
+          expect(game.send(:other_player)).to eq(Game::PLAYER_2)
+        end
+      end
+
+      context 'when `player` is `PLAYER_2`' do
+        it 'returns `PLAYER_1`' do
+          game.player = Game::PLAYER_2
+          expect(game.send(:other_player)).to eq(Game::PLAYER_1)
+        end
       end
     end
   end
